a56d43f6f657eef814a5657d00c85220163b2cba
henadzit <henadzi.tsaryk@gmail.com>
2017-11-24 23:03:51 +0300

    Add blur filter. Add dynamic bg. Tweak params.

--- cam.py
+++ cam.py
@@ -3,29 +3,47 @@ import cv2
 
 
 def main(lower, upper):
-    replace_img = cv2.imread('background.png')
+    #replace_img = cv2.imread('background.png')
+
+    l = np.array(lower)
+    u = np.array(upper)
 
     cap = cv2.VideoCapture(0)
 
+    replace_counter = 0
+    replace_bg = cv2.imread('background.png')
+
+    x = 0.05 # 0.1
+    y = 0.05 # 0.00625
+    f1_kernel = np.asarray([[y, y, y, y, y],
+                            [y, x, x, x, y],
+                            [y, x, x, x, y],
+                            [y, x, x, x, y],
+                            [y, y, y, y, y]])
+
     while(True):
-        # Capture frame-by-frame
         _, frame = cap.read()
 
         #import pdb; pdb.set_trace()
- 
+
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # 111, 31, 68
-        l = np.array(lower)
-        u = np.array(upper)
 
         mask = cv2.inRange(hsv, l, u)
+        #mask = cv2.filter2D(mask, -1, f1_kernel)
+        #mask = cv2.blur(mask, (5,5))
+        mask = cv2.medianBlur(mask, 9)
         mask_inv = cv2.bitwise_not(mask)
 
-        main = cv2.bitwise_and(frame, frame, mask=mask_inv)
-        replaced = cv2.bitwise_and(replace_img, replace_img, mask=mask)
+        bg = cv2.bitwise_and(frame, frame, mask=mask_inv)
+        fg = cv2.bitwise_and(replace_bg, replace_bg, mask=mask)
 
-        cv2.imshow('mask',mask)
-        cv2.imshow('res', cv2.add(main, replaced))
+        # cv2.imshow('mask',mask)
+        cv2.imshow('res', cv2.add(bg, fg))
+
+        if replace_counter % 15 == 0:
+            replace_bg = cv2.add(bg, fg)
+        replace_counter += 1
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -50,4 +68,5 @@ def save_background():
     cap.release()
 
 if __name__ == '__main__':
+    #main(_convert_hsv(48, 6, 5), _convert_hsv(140, 200, 200))
     main(_convert_hsv(63, 10, 5), _convert_hsv(140, 140, 140))
